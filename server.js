2dafda8607a4900ff857637077b42a8d35dd8707
Vishal Katore <vtkatore8459@gmail.com>
2026-03-27 11:44:37 +0530

    Change default HOST to '0.0.0.0'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { requestLogger } = require('./middleware/logger');
 const { errorHandler } = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 
 // Middleware pipeline
 const middlewares = [corsMiddleware, requestLogger];
